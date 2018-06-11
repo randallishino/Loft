2f83d0815865113d16b029070c9a004345df1bf3
randall ishino <rishino24@gmail.com>
2018-06-11 15:23:36 -0700

    change express --> app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(process.env.PORT || 5000, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.listen(process.env.PORT || 5000, () => console.log(`Listening on ${ PORT }`));
