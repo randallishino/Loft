6f16a87f1da5cc561cae8aabb8e6f0c2726aefab
randall ishino <rishino24@gmail.com>
2018-06-11 15:20:53 -0700

    change port

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const path = require('path');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(process.env.PORT || 5000, () => console.log(`Listening on ${ PORT }`))
